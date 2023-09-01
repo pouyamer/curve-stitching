9cdb6022facb0fe183aa6d348afc2610f5890eb8
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-09-01 17:29:04 +0330

    made code more readable

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
 const canvas = document.querySelector(".canvas")
-const c = canvas.getContext("2d")
+const ctx = canvas.getContext("2d")
 const input = document.querySelector(".input")
 
-const size = 500
-
+const size = Math.min(innerHeight, innerWidth)
+let pointCountOnEachAxis = 8
 canvas.width = size
 canvas.height = size
+
+const canvasMultiplier = 1
 
 class Point {
   constructor(x, y) {
     this.x = x
     this.y = y
+    // canvas x and y:
+    this.canvasX = x * canvasMultiplier + canvas.width / 2
+    this.canvasY = -y * canvasMultiplier + canvas.height / 2
+  }
+  lineTo = nextPoint => {
+    ctx.lineWidth = 2
+    ctx.beginPath()
+    ctx.moveTo(this.canvasX, this.canvasY)
+    ctx.lineTo(nextPoint.canvasX, nextPoint.canvasY)
+    ctx.stroke()
+  }
+
+  draw = () => {
+    ctx.beginPath()
+    ctx.arc(this.canvasX, this.canvasY, 5, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+}
+// xMultiplier and yMultiplier = [-1, 0, 1]
+
+const createPointsOnSidesOfAxis = (xMultiplier, yMultiplier) =>
+  Array(pointCountOnEachAxis)
+    .fill("")
+    .map(
+      (_, i) =>
+        new Point(
+          xMultiplier * (i + 1) * (size / (2 * pointCountOnEachAxis)),
+          yMultiplier * (i + 1) * (size / (2 * pointCountOnEachAxis))
+        )
+    )
+
+const center = new Point(0, 0)
+
+const drawCurveStitching = () => {
+  // draw the X and Y axes
+  new Point(-size / 2, 0).lineTo(new Point(size / 2, 0))
+  new Point(0, -size / 2).lineTo(new Point(0, size / 2))
+
+  // making the points on the axes
+  const negativeXPoints = createPointsOnSidesOfAxis(-1, 0)
+  const positiveXPoints = createPointsOnSidesOfAxis(1, 0)
+  const negativeYPoints = createPointsOnSidesOfAxis(0, -1)
+  const positiveYPoints = createPointsOnSidesOfAxis(0, 1)
+
+  // drawing the lines
+  for (let i = 0; i < pointCountOnEachAxis; i++) {
+    positiveYPoints[i].lineTo(negativeXPoints[negativeXPoints.length - i - 1])
+    positiveYPoints[i].lineTo(positiveXPoints[negativeXPoints.length - i - 1])
+    negativeYPoints[i].lineTo(positiveXPoints[negativeXPoints.length - i - 1])
+    negativeYPoints[i].lineTo(negativeXPoints[negativeXPoints.length - i - 1])
   }
 }
 
-const drawLine = (point1, point2) => {
-  c.beginPath()
-  c.moveTo(point1.x, point1.y)
-  c.lineTo(point2.x, point2.y)
-  c.stroke()
-}
-
-const drawShape = pointsOnEachSide => {
-  // Drawing the X Points
-  const Xpoints = [...Array(pointsOnEachSide * 2 + 1)].map(
-    (_, index) => new Point(index * (size / (pointsOnEachSide * 2)), size / 2)
-  )
-
-  // Drawing the Y Points
-  const Ypoints = [...Array(pointsOnEachSide * 2 + 1)].map(
-    (_, index) => new Point(size / 2, index * (size / (pointsOnEachSide * 2)))
-  )
-
-  // Drawing two axes
-  drawLine(new Point(0, size / 2), new Point(size, size / 2))
-  drawLine(new Point(size / 2, 0), new Point(size / 2, size))
-
-  // drawing the Lines
-  Xpoints.forEach((point, index) => {
-    if (index < pointsOnEachSide) {
-      drawLine(point, Ypoints[pointsOnEachSide + index + 1])
-      drawLine(point, Ypoints[pointsOnEachSide - index - 1])
-    }
-    if (index > pointsOnEachSide) {
-      drawLine(point, Ypoints[index - 1 - pointsOnEachSide])
-      drawLine(
-        point,
-        Ypoints[pointsOnEachSide * 2 - (index - 1 - pointsOnEachSide)]
-      )
-    }
-  })
-}
-
-input.addEventListener("input", e => {
-  const points = e.target.value
-  c.clearRect(0, 0, size, size)
-  drawShape(parseInt(points))
+input.addEventListener("change", e => {
+  ctx.clearRect(0, 0, size, size)
+  pointCountOnEachAxis = parseInt(e.target.value)
+  drawCurveStitching()
 })
 
 // Initial draw
-drawShape(parseInt(input.value))
+input.value = pointCountOnEachAxis
+drawCurveStitching(pointCountOnEachAxis)
